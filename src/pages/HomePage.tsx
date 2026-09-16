d63d47102794b686e5ed72b8c84043b9f77e03be
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-16 20:39:00 -0300

    subindo home organizada

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,7 @@ import Noticia from "../components/Noticia/Noticia"
 import Data from "../data/data.json"
 import { Header } from '../components/Header/Header'
 import { Footer } from '../components/Footer/Footer'
+import './HomePage.css'
 
 
 function HomePage() {
@@ -11,7 +12,7 @@ function HomePage() {
             <div>
                 <Header />
                 <main>
-                    <ul>
+                    <ul className="news-list">
                         {Data.map((data) =>
                             <li><Noticia id={data.id} titulo={data.titulo} data={data.data} conteudo={data.conteudo} imagem={data.imagem} categoria={data.categoria} comentarios={data.comentarios} />
                             </li>
