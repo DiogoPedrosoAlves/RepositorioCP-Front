842e2b6cda40e8d86b96b0ae7bcd2167456c1bfe
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-16 20:10:32 -0300

    subindo header e footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Header } from '../src/components/Header/Header'
+import { Footer } from './components/Footer/Footer'
 
 function App() {
   
   return(
   <div>
       <Header/>
-      
+      <main>
+
+      </main>
+      <Footer/>
   </div>
   )
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import './Footer.css'
+
+export function Footer() {
+    return (
+        <>
+            <footer>
+                <p>Samuel Pedroso Xavier - 569335 / Diogo Pedroso Alves - 570024 / Pedro Henrique Sampaio - 571707</p>
+            </footer>
+        </>
+    )
+}
