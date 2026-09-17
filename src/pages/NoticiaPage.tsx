f03de079be98c19c2549d3722d9a75e011dbe8c6
Diogo Pedroso Alves <148377092+DiogoPedrosoAlves@users.noreply.github.com>
2026-09-17 12:05:20 -0300

    Alteração NoticiaPage

--- src/pages/NoticiaPage.tsx
+++ src/pages/NoticiaPage.tsx
@@ -1,8 +1,17 @@
+import { useParams } from "react-router-dom"
 import News from "../components/Noticia/Noticia"
+import data from "../data/data.json"
 
 function NoticiaPage() {
+    const { id } = useParams()
+    const noticia = data.find((item) => item.id === Number(id))
+
+    if (!noticia) {
+        return <p>Notícia não encontrada.</p>
+    }
+
     return (
-        <News />
+        <News {...noticia} />
     )
 }
 
